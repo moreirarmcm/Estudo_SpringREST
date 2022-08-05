e8471d99fc61b3d1d62effdb776fbce0a5443ca5
Renan Moreira <105755795+moreirarmcm@users.noreply.github.com>
2022-08-05 11:49:52 -0300

    Update Cliente

--- I - Injeção de Dependências/modelo/Cliente.java
+++ I - Injeção de Dependências/modelo/Cliente.java
@@ -1,49 +1,46 @@
-package view;
-
-import services.AtivacaoClienteService;
+package com.estudandospring.di.modelo;
 
 public class Cliente {
 	private String nome;
 	private String email;
 	private String telefone;
-	public String getTelefone() {
-		return telefone;
-	}
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-	private int idade;
-	//AtivacaoClienteService acs = new AtivacaoClienteService();
 	
-	
-	public Cliente(String nome, String email, String telefone, int idade) {
+	public Cliente(String nome, String email, String telefone) {
 		super();
 		setNome(nome);
 		setEmail(email);
 		setTelefone(telefone);
-		setIdade(idade);
+		System.out.println("Cliente!");
 	}
+	
 	public void ativar() {
 		System.out.println("Ativando cliente...\n");
 	}
 	
+	
+//==============Getters n' Setters============
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public int getIdade() {
-		return idade;
+
+	public String getTelefone() {
+		return telefone;
 	}
-	public void setIdade(int idade) {
-		this.idade = idade;
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
 	}
 	
 	
